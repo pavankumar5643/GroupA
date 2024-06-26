407a5564332ff21dddd4fb778f8e682503512834
pavankumar5643 <158595727+pavankumar5643@users.noreply.github.com>
2024-06-26 11:46:27 +0530

    Add files via upload

---
+++ VariableDemo2.java
@@ -0,0 +1,22 @@
+package variableDemo;
+
+public class VariableDemo2 
+{
+	int i = 10;
+	static int j = 20;
+	
+	public static void main(String[] args) 
+	{
+		System.out.println(j);
+		j=30;
+		System.out.println(j);
+		int j = 40;
+		System.out.println(j);
+		
+		//System.out.println(k);	
+		VariableDemo2 i = new VariableDemo2();	//datatype variable = object
+		System.out.println(i);
+
+	}
+
+}
---
+++ variableDemo1.java
@@ -0,0 +1,30 @@
+package variableDemo;
+
+public class variableDemo1 
+{
+	int i = 10;
+	static int j = 20;
+	
+		public static void main(String[] args)
+	/*{
+DEMO
+		System.out.println(5);	//5
+		System.out.println("5");	//5
+		System.out.println("5+5");	//5+5
+		System.out.println(5+5);	//10
+		int a=10;
+		System.out.println(a);	//10
+		System.out.println("a");	//a
+		System.out.println(a+5);	//10
+		System.out.println("a+5");	//a+5
+	}*/
+		{
+			System.out.println(j);
+			j = 30;
+			System.out.println(j);
+			int j = 40;
+			System.out.println(j);
+			//System.out.println(i); 	//run time Error
+		}
+
+}
